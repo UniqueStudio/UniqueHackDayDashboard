d37892ef26f5b670f9cf00b941583b97c874eb7c
Faraway <hzylovelyl@gmail.com>
2018-03-19 17:27:28 +0800

    feat: simpfiy saga

--- src/redux/sagas/index.ts
+++ src/redux/sagas/index.ts
@@ -10,61 +10,40 @@ export function* watchFetchData() {
 import { put } from 'redux-saga/effects';
 import { RootState, AnyAction } from '../reducers/index';
 import request from '../../lib/API';
-import { UserEntryData } from '../reducers/userEntry';
 
 const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 const getUserEntry = (state: RootState) => state.userEntry;
+const map = {
+  regUsername: ['username', '用户名'],
+  regEmail: ['email', '邮箱'],
+};
 
 export function* checkExistence(action: AnyAction) {
   yield delay(500);
 
-  const { regUsername, regEmail }: UserEntryData = yield select(getUserEntry);
+  const userEntry = yield select(getUserEntry);
+  const { fieldName, changedTo } = action.payload;
 
-  if (action.payload.fieldName === 'regUsername') {
-    if (
-      regUsername.validateStatus === 'error' &&
-      regUsername.help &&
-      regUsername.help.indexOf('已存在') <= 0
-    ) {
-      return;
-    }
-    const result = yield request({
-      endpoint: '/v1/user/existence?type=username',
-      method: 'GET',
-      body: { valueToCheck: action.payload.changedTo },
+  if (
+    userEntry[fieldName].validateStatus === 'error' &&
+    userEntry[fieldName].help &&
+    userEntry[fieldName].help.indexOf('已存在') <= 0
+  ) {
+    return;
+  }
+  const result = yield request({
+    endpoint: `/v1/user/existence?type=${(map as any)[fieldName][0]}`,
+    method: 'GET',
+    body: { valueToCheck: changedTo },
+  } as any);
+  if (result.data.existence) {
+    yield put({
+      type: 'CHANGE_USER_ENTRY_TIP',
+      payload: {
+        fieldName,
+        validateStatus: 'error',
+        help: `${(map as any)[fieldName][1]}已存在`,
+      },
     });
-    if (result.data.existence) {
-      yield put({
-        type: 'CHANGE_USER_ENTRY_TIP',
-        payload: {
-          fieldName: action.payload.fieldName,
-          validateStatus: 'error',
-          help: '用户名已存在',
-        },
-      });
-    }
-  } else if (action.payload.fieldName === 'regEmail') {
-    if (
-      regEmail.validateStatus === 'error' &&
-      regEmail.help &&
-      regEmail.help.indexOf('已存在') <= 0
-    ) {
-      return;
-    }
-    const result = yield request({
-      endpoint: '/v1/user/existence?type=email',
-      method: 'GET',
-      body: { valueToCheck: action.payload.changedTo },
-    });
-    if (result.data.existence) {
-      yield put({
-        type: 'CHANGE_USER_ENTRY_TIP',
-        payload: {
-          fieldName: action.payload.fieldName,
-          validateStatus: 'error',
-          help: '邮箱已存在',
-        },
-      });
-    }
   }
 }
