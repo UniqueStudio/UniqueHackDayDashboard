860a7284871c4f8ce81c9e5c34c4e744c2cc2954
Faraway <hzylovelyl@gmail.com>
2018-04-03 09:30:41 +0800

    fix: fix paths.js

--- scripts/paths.js
+++ scripts/paths.js
@@ -1,8 +1,8 @@
 const path = require('path');
-
+// const config = require('../webpack.config.prod');
 const paths = {
   appBuild: path.resolve(__dirname, '../dist'),
-  appPublic: config.output.publicPath,
+  appPublic: '/',
   appPackageJson: path.resolve(__dirname, '../package.json'),
   yarnLockFile: path.resolve(__dirname, '../yarn.lock'),
 };
