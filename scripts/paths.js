9860f5242e6a6f93ab60bf4e3cb7fc798e5fd849
Faraway <hzylovelyl@gmail.com>
2018-04-03 12:44:54 +0800

    fix: CI publicPath

--- scripts/paths.js
+++ scripts/paths.js
@@ -1,7 +1,10 @@
 const path = require('path');
 const paths = {
   appBuild: path.resolve(__dirname, '../build'),
-  appPublic: process.env.CI === 'true' ? 'https://console.fredliang.cn/prod/' : '/',
+  appPublic:
+    process.env.CI === 'true' && process.env.NODE_ENV === 'production'
+      ? 'https://console.fredliang.cn/prod/'
+      : '/',
   appPackageJson: path.resolve(__dirname, '../package.json'),
   yarnLockFile: path.resolve(__dirname, '../yarn.lock'),
 };
