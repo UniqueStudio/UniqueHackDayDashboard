02f2fbb7b74881e55fc535ec85f3373c7c1e8838
Faraway <hzylovelyl@gmail.com>
2018-04-05 01:27:47 +0800

    feat: add svg support in webpack config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -83,7 +83,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpg|gif)$/,
+        test: /\.(png|jpg|gif|svg)$/,
         use: [
           {
             loader: 'file-loader',
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -79,7 +79,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpg|gif)$/,
+        test: /\.(png|jpg|gif|svg)$/,
         use: [
           {
             loader: 'file-loader',
