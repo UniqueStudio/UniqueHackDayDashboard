f4f4c39c9e1f55d88abebad20564bf2e861d9f12
llgzone <985395976@qq.com>
2018-04-18 03:35:22 +0800

    feat: add special console log

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import 'regenerator-runtime/runtime';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Loadable from 'react-loadable';
+import './lib/specialLog';
 
 // import store from './redux/store/index';
 // import * as TYPE from './redux/actions';
