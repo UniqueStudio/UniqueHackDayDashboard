f90bdeee6ac9ee9a690276d5dfee529ec60a494c
Faraway <hzylovelyl@gmail.com>
2018-04-22 02:12:49 +0800

    refactor: add entry file for server

---
+++ server/src/index.ts
@@ -0,0 +1,38 @@
+import { port, host } from '../config';
+// import jsonwebtoken from 'jsonwebtoken';
+import WebSocket from 'ws';
+import http from 'http';
+import url from 'url';
+import querystring from 'querystring';
+import uuid from 'uuid';
+
+import Client from './client/index';
+
+const clients: { [k: string]: Client } = {};
+const server = http.createServer();
+const wss = new WebSocket.Server({ noServer: true });
+
+server.on('upgrade', (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, ws => {
+    wss.emit('connection', ws);
+
+    const { query } = url.parse(request.url);
+    if (query) {
+      const params = querystring.parse(query);
+      if (params.clientId && typeof params.clientId === 'string' && clients[params.clientId]) {
+        const oldClient = clients[params.clientId];
+        if (oldClient.reuseable) {
+          oldClient.replaceWebSocket(ws);
+          return;
+        }
+      }
+    }
+
+    const newClient = new Client(ws);
+    const clientId = uuid();
+    ws.send(JSON.stringify({ clientId }));
+    clients[clientId] = newClient;
+  });
+});
+
+server.listen(port, host);
