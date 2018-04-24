534efab90530f2c3126967a8a6c2d2e79a7f5dbd
llgzone <985395976@qq.com>
2018-04-25 04:44:35 +0800

    fix: fix conflict

--- src/redux/reducers/admin/index.ts
+++ src/redux/reducers/admin/index.ts
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
 import userState, { UserStatus } from './userStatus';
-=======
-import userState, { userStatus } from './userStatus';
->>>>>>> 650e4e6cc98759f7ec25ec364b121508f31770df
 import { combineReducers } from 'redux';
 import userVerify, { AdminUser } from './userVerify';
 
 export interface Admin {
   users: AdminUser;
-<<<<<<< HEAD
   userState: UserStatus;
-=======
-  userState: userStatus;
->>>>>>> 650e4e6cc98759f7ec25ec364b121508f31770df
 }
 
 export default combineReducers({
   users: userVerify,
-<<<<<<< HEAD
   userState,
-=======
-  userState: userState,
->>>>>>> 650e4e6cc98759f7ec25ec364b121508f31770df
 });
