9e9bbf8a611c259011ca4b12890f5a49d4447153
llgzone <985395976@qq.com>
2018-04-28 22:24:49 +0800

    feat: add current pass and fix bug

--- src/Views/Admin/UserVerify/index.tsx
+++ src/Views/Admin/UserVerify/index.tsx
@@ -11,25 +11,19 @@ import Dvider from 'antd/es/divider';
 
 export interface UserVerifyProps {
   data: AdminUser['items'];
-  statusChange(username: string, state: 2 | 3): { type: string; username: string; state: 2 | 3 };
+  statusChange(username: string, state: 0 | 1): { type: string; username: string; state: 0 | 1 };
   isSubmitting: boolean;
   stateChangeSubmit: () => { type: string };
   isSuperAdmin: boolean;
+  currentPass: number;
 }
 
 class UserVerify extends React.Component<UserVerifyProps> {
-  state = {
-    total: this.props.data.length,
-    // 审核中人数
-    pengdingNum: this.props.data.filter(user => user.verifyState === 0).length,
-    rejectNum: this.props.data.filter(user => user.verifyState === 1).length,
-  };
-
   renderOperation = (record: any) => {
     return (
       <React.Fragment>
         {/* tslint:disable-next-line:jsx-no-lambda */}
-        <Radio.Group onChange={e => this.props.statusChange(record.name, e.target.value)}>
+        <Radio.Group onChange={e => this.props.statusChange(record.username, e.target.value)}>
           <Radio value={1}>通过</Radio>
           <Radio value={0}>拒绝</Radio>
         </Radio.Group>
@@ -41,9 +35,9 @@ class UserVerify extends React.Component<UserVerifyProps> {
     return (
       <React.Fragment>
         <Dvider type="vertical" />
-        <span>已通过: {passNum}</span>
-        <Dvider type="vertical" />
         <span>审核中: {pengdingNum}</span>
+        <Dvider type="vertical" />
+        <span>已通过: {passNum}</span>
         <Dvider type="vertical" />
         <span>未通过: {rejectNum}</span>
       </React.Fragment>
@@ -51,11 +45,21 @@ class UserVerify extends React.Component<UserVerifyProps> {
   }
 
   renderFooter = () => {
-    const { total, pengdingNum, rejectNum } = this.state;
+    const total = this.props.data.length;
+    // 审核中人数
+    const pengdingNum = this.props.data.filter(
+      user => user.verifyState === 0 || user.verifyState === 1,
+    ).length;
+    const rejectNum = this.props.data.filter(user => user.verifyState === 2).length;
     const passNum = total - pengdingNum - rejectNum;
+
     return (
       <React.Fragment>
-        <span>总数: {this.state.total}</span>
+        <span>总数: {total}</span>
+        <Dvider type="vertical" />
+        <span>
+          当前通过: {this.props.currentPass}/{total}
+        </span>
         {this.props.isSuperAdmin && this.renderSuperAdminFooter(passNum, pengdingNum, rejectNum)}
       </React.Fragment>
     );
@@ -68,9 +72,9 @@ class UserVerify extends React.Component<UserVerifyProps> {
   render() {
     const Column = Table.Column;
     const statusFilters = [
-      { text: '审核中', value: '0' },
-      { text: '未通过', value: '1' },
-      { text: '已通过', value: '2' },
+      { text: '审核中', value: '0' || '1' },
+      { text: '未通过', value: '2' },
+      { text: '已通过', value: '3' },
     ];
 
     return (
@@ -143,11 +147,12 @@ const mapStateToProps = (state: RootState) => {
     isSubmitting: state.admin.userState.isSubmitting,
     data: state.admin.users.items,
     isSuperAdmin: state.user.permission === 2,
+    currentPass: state.admin.userState.value.length,
   };
 };
 
 export default connect(mapStateToProps, {
-  statusChange(username: string, state: 2 | 3) {
+  statusChange(username: string, state: 0 | 1) {
     return {
       type: TYPE.ADMIN_USER_STATUS_CHANGE._,
       username,
