a020c534dc4f5746235050fbf4c47bee52b89311
ttzztztz <935319340@qq.com>
2019-05-06 16:51:01 +0800

    fix

--- src/Components/MyForm/MobileCode.tsx
+++ src/Components/MyForm/MobileCode.tsx
@@ -12,94 +12,98 @@ import Col from 'antd/es/col';
 import { patterns } from '../../lib/patterns';
 
 export interface MobileCodeProps {
-  isReady?: boolean;
-  isSending?: boolean;
-  validateFileds?: string[];
+    isReady?: boolean;
+    isSending?: boolean;
+    validateFileds?: string[];
 
-  onSend: () => Promise<any>;
+    onSend: () => Promise<any>;
 }
 
 export default class MobileCode extends React.Component<MobileCodeProps> {
-  state = {
-    count: 0,
-  };
+    state = {
+        count: 0,
+    };
 
-  timer = 0;
+    timer = 0;
 
-  reallyRequestSMS = async () => {
-    const { form } = this.context;
-    const { validateFileds = ['phone'] } = this.props;
+    reallyRequestSMS = async () => {
+        const { form } = this.context;
+        const { validateFileds = ['phone'] } = this.props;
 
-    const err = await new Promise(resolve => {
-      form.validateFields(validateFileds, resolve);
-    });
+        const err = await new Promise(resolve => {
+            form.validateFields(validateFileds, resolve);
+        });
 
-    if (err) {
-      return;
+        if (err) {
+            return;
+        }
+
+        let count = 59;
+        await this.props.onSend();
+        this.setState({ count });
+        this.timer = window.setInterval(() => {
+            count -= 1;
+            this.setState({ count });
+            if (count === 0) {
+                clearInterval(this.timer);
+            }
+        }, 1000);
+    };
+
+    render() {
+        const {
+            form: { getFieldDecorator },
+        } = this.context;
+        const { count } = this.state;
+        return (
+            <Form.Item hasFeedback={false}>
+                <Row gutter={8}>
+                    <Col span={16}>
+                        {getFieldDecorator('code', {
+                            rules: [
+                                { required: true, message: '必须输入验证码' },
+                                { pattern: patterns.code, message: '验证码格式不正确' },
+                            ],
+                        })(
+                            <Input
+                                size="large"
+                                placeholder="请输入短信验证码"
+                                prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,0.25)' }} />}
+                            />,
+                        )}
+                    </Col>
+                    <Col span={8}>
+                        <Button
+                            style={{ width: '100%', fontSize: '1.5vw' }}
+                            size="large"
+                            disabled={
+                                !!this.state.count || this.props.isReady || this.props.isSending
+                            }
+                            onClick={this.reallyRequestSMS}
+                        >
+                            {this.props.isSending ? (
+                                <Icon type="loading" />
+                            ) : !this.props.isReady ? (
+                                count ? (
+                                    `${count} s`
+                                ) : (
+                                    '获取验证码'
+                                )
+                            ) : (
+                                '正在加载'
+                            )}
+                        </Button>
+                    </Col>
+                </Row>
+            </Form.Item>
+        );
     }
 
-    let count = 59;
-    await this.props.onSend();
-    this.setState({ count });
-    this.timer = window.setInterval(() => {
-      count -= 1;
-      this.setState({ count });
-      if (count === 0) {
-        clearInterval(this.timer);
-      }
-    }, 1000);
-  };
+    componentWillUnmount() {
+        window.clearInterval(this.timer);
+    }
 
-  render() {
-    const { form: { getFieldDecorator } } = this.context;
-    const { count } = this.state;
-    return (
-      <Form.Item hasFeedback={false}>
-        <Row gutter={8}>
-          <Col span={16}>
-            {getFieldDecorator('code', {
-              rules: [
-                { required: true, message: '必须输入验证码' },
-                { pattern: patterns.code, message: '验证码格式不正确' },
-              ],
-            })(
-              <Input
-                size="large"
-                placeholder="请输入短信验证码"
-                prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,0.25)' }} />}
-              />,
-            )}
-          </Col>
-          <Col span={8}>
-            <Button
-              style={{ width: '100%' }}
-              size="large"
-              disabled={!!this.state.count || this.props.isReady || this.props.isSending}
-              onClick={this.reallyRequestSMS}
-            >
-              {this.props.isSending ? (
-                <Icon type="loading" />
-              ) : !this.props.isReady ? (
-                count ? (
-                  `${count} s`
-                ) : (
-                  '获取验证码'
-                )
-              ) : (
-                '正在加载'
-              )}
-            </Button>
-          </Col>
-        </Row>
-      </Form.Item>
-    );
-  }
-
-  componentWillUnmount() {
-    window.clearInterval(this.timer);
-  }
-
-  static contextTypes = {
-    form: PropTypes.object,
-  };
+    static contextTypes = {
+        form: PropTypes.object,
+    };
 }
--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,3 +1,5 @@
 // export const hostname =
 //   process.env.TRAVIS_BRANCH === 'master' ? 'deploy.fredliang.cn' : 'backend.fredliang.cn';
 export const hostname = 'hd.plus1sec.cn';
+
+// export const hostname = 'hackday.outsiders.top';
