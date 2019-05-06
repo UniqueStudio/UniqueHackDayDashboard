5f3264b8703596dac3cfa13097eaae7891776d5a
ttzztztz <935319340@qq.com>
2019-05-06 22:50:36 +0800

    update  shedule

--- src/Components/HackdayProgress/index.tsx
+++ src/Components/HackdayProgress/index.tsx
@@ -9,11 +9,12 @@ const HackdayProgress = () => {
     return (
         <Card bordered={false} title="比赛进程">
             <Steps progressDot={true} direction="vertical">
-                <Steps.Step title="2019/5/7" description="开始报名" />
-                <Steps.Step title="2019/5/30" description="停止报名" />
-                <Steps.Step title="2019/6/1" description="简历筛选结果公布，确认是否参赛" />
-                <Steps.Step title="2019/6/8" description="开幕式&开始比赛" />
-                <Steps.Step title="2019/6/9" description="结束比赛&答辩&颁奖仪式" />
+                <Steps.Step title="5月7日" description="开始报名" />
+                <Steps.Step title="5月7日 - 5月30日" description="筛选简历" />
+                <Steps.Step title="5月30日" description="停止报名" />
+                <Steps.Step title="6月1日 - 6月2日" description="确认是否参赛" />
+                <Steps.Step title="6月8日" description="开幕式&开始比赛" />
+                <Steps.Step title="6月9日" description="结束比赛&答辩&颁奖仪式" />
             </Steps>
         </Card>
     );
