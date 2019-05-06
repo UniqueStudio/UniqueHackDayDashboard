fe7cf42e8d826b87c5a20f0217c0e66a5119e465
ttzztztz <935319340@qq.com>
2019-05-06 12:36:41 +0800

    Competition Progress Update

--- src/Components/HackdayProgress/index.tsx
+++ src/Components/HackdayProgress/index.tsx
@@ -6,17 +6,17 @@ import Card from 'antd/es/card';
 // import cls from '../Status/style.less';
 
 const HackdayProgress = () => {
-  return (
-    <Card bordered={false} title="比赛进程">
-      <Steps progressDot={true} direction="vertical">
-        <Steps.Step title="2019/4/17" description="开始报名" />
-        <Steps.Step title="2019/5/20" description="停止报名" />
-        <Steps.Step title="2019/5/25" description="确认是否参赛" />
-        <Steps.Step title="2019/6/1" description="开幕式，Hackday 比赛开始" />
-        <Steps.Step title="2019/6/2" description="作品提交，评分，闭幕式&颁奖" />
-      </Steps>
-    </Card>
-  );
+    return (
+        <Card bordered={false} title="比赛进程">
+            <Steps progressDot={true} direction="vertical">
+                <Steps.Step title="2019/5/7" description="开始报名" />
+                <Steps.Step title="2019/5/30" description="停止报名" />
+                <Steps.Step title="2019/6/1" description="简历筛选结果公布，确认是否参赛" />
+                <Steps.Step title="2019/6/8" description="开幕式&开始比赛" />
+                <Steps.Step title="2019/6/9" description="结束比赛&答辩&颁奖仪式" />
+            </Steps>
+        </Card>
+    );
 };
 
 export default HackdayProgress;
